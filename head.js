5905dddd33b206ca6c7da90b43b03a5a9ffaf574
Ethan <ejholmes@buffalo.edu>
2025-03-18 21:58:20 -0400

    adsense header

--- head.js
+++ head.js
@@ -6,4 +6,6 @@ document.write(`
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dokkanle!</title>
     <link rel="stylesheet" href="${p}styles.css?v=2.2">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5334808006574572"
+     crossorigin="anonymous"></script>
     `);
